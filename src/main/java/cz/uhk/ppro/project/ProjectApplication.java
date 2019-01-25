304fdf6dc28436200a320fa123b8f8d8f24dba0c
kolommi1 <kolommi1@uhk.cz>
2019-01-25 21:49:28 +0100

    Fix

--- src/main/java/cz/uhk/ppro/project/ProjectApplication.java
+++ src/main/java/cz/uhk/ppro/project/ProjectApplication.java
@@ -1,17 +1,9 @@
 package cz.uhk.ppro.project;
 
-//import cz.uhk.ppro.project.services.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-/*
-@EnableConfigurationProperties({
-        FileStorageProperties.class
-})
-*/
-
 public class ProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
